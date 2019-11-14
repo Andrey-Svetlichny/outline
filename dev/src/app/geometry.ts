a9775cb5d9ec21c6712ba8e408ea1aca0bf3de99
Andrey Svetlichny <Andrey.Svetlichny@gmail.com>
2019-11-14 15:29:16 +0100

    Geometry: add pointToLineDistance

--- dev/src/app/geometry.ts
+++ dev/src/app/geometry.ts
@@ -29,6 +29,23 @@ export function lineTouch(l1: ILine, l2: ILine) {
     return equalPoints(l1.p1, l2.p1) || equalPoints(l1.p1, l2.p2) || equalPoints(l1.p2, l2.p1) || equalPoints(l1.p2, l2.p2);
   }
 
+
+function sqr(x) { return x * x }
+function dist2(v, w) { return sqr(v.x - w.x) + sqr(v.y - w.y) }
+function distToSegmentSquared(p, v, w) {
+  var l2 = dist2(v, w);
+  if (l2 == 0) return dist2(p, v);
+  var t = ((p.x - v.x) * (w.x - v.x) + (p.y - v.y) * (w.y - v.y)) / l2;
+  t = Math.max(0, Math.min(1, t));
+  return dist2(p, { x: v.x + t * (w.x - v.x),
+    y: v.y + t * (w.y - v.y) });
+}
+export function pointToLineDistance(p: IPoint, line: ILine) {
+  const v = line.p1;
+  const w = line.p2;
+  return Math.sqrt(distToSegmentSquared(p, v, w));
+}
+
 /**
  * Are two line segments intersect?
  * Uses the vector cross product approach described below:
@@ -67,8 +84,8 @@ export function lineIntersects(l1: ILine, l2: ILine) {
 }
 
 
-export function segmentsIntersectionPoint(p1: IPoint, p2: IPoint, p3: IPoint, p4: IPoint): IPoint {
-  const x1=p1.x, y1=p1.y, x2=p2.x, y2=p2.y, x3=p3.x, y3=p3.y, x4=p4.x, y4=p4.y;
+export function lineIntersectionPoint(l1: ILine, l2: ILine): IPoint {
+  const x1=l1.p1.x, y1=l1.p1.y, x2=l1.p2.x, y2=l1.p2.y, x3=l2.p1.x, y3=l2.p1.y, x4=l2.p2.x, y4=l2.p2.y;
   const px= ( (x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4) ) / ( (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4) )
   const py= ( (x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4) ) / ( (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4) )
   return {x: px, y: py};
